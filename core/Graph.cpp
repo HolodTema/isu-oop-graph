dbd80ef1b484b0d1e5d6ae174239791630754ed1
artem <art.holodilov@gmail.com>
2026-05-17 20:32:24 +0300

    Fixed graph output operator

--- core/Graph.cpp
+++ core/Graph.cpp
@@ -1,87 +1,87 @@
 #include "Graph.hpp"
+#include <iomanip>
+#include <iostream>
 
-
-void Graph::addNode(Node* node) {
-    for (Node::iterator it = node->neighboursBegin(); it != node->neighboursEnd(); it++) {
-        (*it)->addNeighbour(node);
-    }
-    nodes_.insert(node);
+void Graph::addNode(Node *node) {
+	for (Node::iterator it = node->neighboursBegin(); it != node->neighboursEnd(); it++) {
+		(*it)->addNeighbour(node);
+	}
+	nodes_.insert(node);
 }
 
 
-void Graph::removeNode(Node* node) {
-    //remove this node from set of graph nodes
-    nodes_.erase(node);
+void Graph::removeNode(Node *node) {
+	//remove this node from set of graph nodes
+	nodes_.erase(node);
 
-    //for other nodes we need to delete this node from its neighbours
-    for (Node::iterator it = nodes_.begin(); it != nodes_.end(); ++it) {
-        (*it)->removeNeighbour(node);
-    }
+	//for other nodes we need to delete this node from its neighbours
+	for (Node::iterator it = nodes_.begin(); it != nodes_.end(); ++it) {
+		(*it)->removeNeighbour(node);
+	}
 }
 
 
-void Graph::addEdge(Node* begin, Node* end) {
-    //if there are no begin or end node in the nodes_
-    //then we 
-    if (nodes_.find(begin) == nodes_.end()) {
-        return;
-    }
-    if (nodes_.find(end) == nodes_.end()) {
-        return;
-    }
+void Graph::addEdge(Node *begin, Node *end) {
+	//if there are no begin or end node in the nodes_
+	//then we
+	if (nodes_.find(begin) == nodes_.end()) {
+		return;
+	}
+	if (nodes_.find(end) == nodes_.end()) {
+		return;
+	}
 
-    //bind begin and end nodes
-    begin->addNeighbour(end);
-    end->addNeighbour(begin);
+	//bind begin and end nodes
+	begin->addNeighbour(end);
+	end->addNeighbour(begin);
 }
 
 
-void Graph::removeEdge(Node* begin, Node* end) {
-    //if there are no begin or end node in the nodes_
-    //then we 
-    if (nodes_.find(begin) == nodes_.end()) {
-        return;
-    }
-    if (nodes_.find(end) == nodes_.end()) {
-        return;
-    }
+void Graph::removeEdge(Node *begin, Node *end) {
+	//if there are no begin or end node in the nodes_
+	//then we
+	if (nodes_.find(begin) == nodes_.end()) {
+		return;
+	}
+	if (nodes_.find(end) == nodes_.end()) {
+		return;
+	}
 
-    //remove binding between end and begin nodes
-    begin->removeNeighbour(end);
-    end->removeNeighbour(begin);
+	//remove binding between end and begin nodes
+	begin->removeNeighbour(end);
+	end->removeNeighbour(begin);
 }
 
 
 Node::iterator Graph::begin() const {
-    return nodes_.begin();
+	return nodes_.begin();
 }
 
 
 Node::iterator Graph::end() const {
-    return nodes_.end();
+	return nodes_.end();
 }
 
-std::ostream& operator<<(std::ostream& os, const Graph& graph) {
-    std::ostream::sentry s(os);
-    if (!s) {
-        return os;
-    }
+std::ostream &operator<<(std::ostream &os, const Graph &graph) {
+	std::ostream::sentry s(os);
+	if (!s) {
+		return os;
+	}
 
-    std::set<Node*> setPrintedSourceNodes;
+	std::set<Node *> setPrintedSourceNodes;
 
-    os << "Source  Target\n";
-    for (auto it = graph.begin(); it != graph.end(); ++it) {
-        Node* nodeSource = *it;
-        for (auto it2 = nodeSource->neighboursBegin(); it2 != nodeSource->neighboursEnd(); ++it2) {
-            Node* nodeTarget = *it2;
-            if (setPrintedSourceNodes.find(nodeTarget) != setPrintedSourceNodes.end()) {
-                continue;
-            }
-            os << nodeSource->getName() << "       " << nodeTarget->getName() << "\n";
-        }
-        setPrintedSourceNodes.insert(nodeSource);
-    }
+	os << "Source  Target\n";
+	for (auto it = graph.begin(); it != graph.end(); ++it) {
+		Node *nodeSource = *it;
+		for (auto it2 = nodeSource->neighboursBegin(); it2 != nodeSource->neighboursEnd(); ++it2) {
+			Node *nodeTarget = *it2;
+			if (setPrintedSourceNodes.find(nodeTarget) != setPrintedSourceNodes.end()) {
+				continue;
+			}
+			os << std::left << std::setw(8) << nodeSource->getName() << std::setw(8) << nodeTarget->getName() << "\n";
+		}
+		setPrintedSourceNodes.insert(nodeSource);
+	}
 
-    return os;
+	return os;
 }
-
--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+
+#include "bfs/BFS.hpp"
 #include "core/Graph.hpp"
 
-
-int main() {
+void testBFS() {
+    std::cout << "Test BFS algorithm:\n";
     Node node0("0");
     Node node1("1");
     Node node2("2");
     Node node3("3");
     Node node4("4");
     Node node5("5");
-    
+
     Graph graph;
     graph.addNode(&node0);
     graph.addNode(&node1);
@@ -18,16 +20,68 @@ int main() {
     graph.addNode(&node4);
     graph.addNode(&node5);
 
-    graph.addEdge(&node0, &node1);
     graph.addEdge(&node0, &node3);
     graph.addEdge(&node1, &node2);
     graph.addEdge(&node0, &node5);
     graph.addEdge(&node3, &node4);
     graph.addEdge(&node4, &node5);
-    graph.addEdge(&node2, &node5);
-    
-    std::cout << graph;
 
+    std::cout << "Created graph (disconnected in two subgraphs 1,2 and 0,3,4,5):\n" << graph << "\n";
+    BFS bfs(graph);
+    std::cout << "Have way between 0 and 0 (expected true):\t" << bfs.connected(&node0, &node0) << "\n";
+    std::cout << "Have way between 0 and 1 (expected false):\t" << bfs.connected(&node0, &node1) << "\n";
+    std::cout << "Have way between 0 and 2 (expected false):\t" << bfs.connected(&node0, &node2) << "\n";
+    std::cout << "Have way between 0 and 3 (expected true):\t" << bfs.connected(&node0, &node3) << "\n";
+    std::cout << "Have way between 0 and 4 (expected true):\t" << bfs.connected(&node0, &node4) << "\n";
+    std::cout << "Have way between 0 and 5 (expected true):\t" << bfs.connected(&node0, &node5) << "\n";
+    std::cout << "\n";
+    std::cout << "Have way between 1 and 0 (expected false):\t" << bfs.connected(&node1, &node0) << "\n";
+    std::cout << "Have way between 1 and 1 (expected true):\t" << bfs.connected(&node1, &node1) << "\n";
+    std::cout << "Have way between 1 and 2 (expected true):\t" << bfs.connected(&node1, &node2) << "\n";
+    std::cout << "Have way between 1 and 3 (expected false):\t" << bfs.connected(&node1, &node3) << "\n";
+    std::cout << "Have way between 1 and 4 (expected false):\t" << bfs.connected(&node1, &node4) << "\n";
+    std::cout << "Have way between 1 and 5 (expected false):\t" << bfs.connected(&node1, &node5) << "\n";
+    std::cout << "\n";
+    std::cout << "Have way between 2 and 0 (expected false):\t" << bfs.connected(&node2, &node0) << "\n";
+    std::cout << "Have way between 2 and 1 (expected true):\t" << bfs.connected(&node2, &node1) << "\n";
+    std::cout << "Have way between 2 and 2 (expected true):\t" << bfs.connected(&node2, &node2) << "\n";
+    std::cout << "Have way between 2 and 3 (expected false):\t" << bfs.connected(&node2, &node3) << "\n";
+    std::cout << "Have way between 2 and 4 (expected false):\t" << bfs.connected(&node2, &node4) << "\n";
+    std::cout << "Have way between 2 and 5 (expected false):\t" << bfs.connected(&node2, &node5) << "\n";
+    std::cout << "\n";
+    std::cout << "Have way between 3 and 0 (expected true):\t" << bfs.connected(&node3, &node0) << "\n";
+    std::cout << "Have way between 3 and 1 (expected false):\t" << bfs.connected(&node3, &node1) << "\n";
+    std::cout << "Have way between 3 and 2 (expected false):\t" << bfs.connected(&node3, &node2) << "\n";
+    std::cout << "Have way between 3 and 3 (expected true):\t" << bfs.connected(&node3, &node3) << "\n";
+    std::cout << "Have way between 3 and 4 (expected true):\t" << bfs.connected(&node3, &node4) << "\n";
+    std::cout << "Have way between 3 and 5 (expected true):\t" << bfs.connected(&node3, &node5) << "\n";
+    std::cout << "\n";
+    std::cout << "Have way between 4 and 0 (expected true):\t" << bfs.connected(&node4, &node0) << "\n";
+    std::cout << "Have way between 4 and 1 (expected false):\t" << bfs.connected(&node4, &node1) << "\n";
+    std::cout << "Have way between 4 and 2 (expected false):\t" << bfs.connected(&node4, &node2) << "\n";
+    std::cout << "Have way between 4 and 3 (expected true):\t" << bfs.connected(&node4, &node3) << "\n";
+    std::cout << "Have way between 4 and 4 (expected true):\t" << bfs.connected(&node4, &node4) << "\n";
+    std::cout << "Have way between 4 and 5 (expected true):\t" << bfs.connected(&node4, &node5) << "\n";
+    std::cout << "\n";
+    std::cout << "Have way between 5 and 0 (expected true):\t" << bfs.connected(&node5, &node0) << "\n";
+    std::cout << "Have way between 5 and 1 (expected false):\t" << bfs.connected(&node5, &node1) << "\n";
+    std::cout << "Have way between 5 and 2 (expected false):\t" << bfs.connected(&node5, &node2) << "\n";
+    std::cout << "Have way between 5 and 3 (expected true):\t" << bfs.connected(&node5, &node3) << "\n";
+    std::cout << "Have way between 5 and 4 (expected true):\t" << bfs.connected(&node5, &node4) << "\n";
+    std::cout << "Have way between 5 and 5 (expected true):\t" << bfs.connected(&node5, &node5) << "\n";
+}
+
+void testBfsFromFile() {
+    Graph graph("./assets/testgraph.txt");
+    std::cout << "Graph from file testgraph.txt:\n";
+    std::cout << graph << "\n";
+    for (auto it = graph.begin(); it != graph.end(); ++it) {
+
+    }
+}
+
+int main() {
+    testBfsFromFile();
     return 0;
 }
 
